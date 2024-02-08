85ab375d21d07fb6b4f3f8073f1024baeca8fffd
Marcio de Andrade <gmandrade1@hotmail.com>
2024-02-08 12:13:57 -0400

    preparação do projeto inicial

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Container } from '../../styles';
+
+const Button = () => {
+return(
+    <Container></Container>
+);
+
+}
+
+export default Button;
